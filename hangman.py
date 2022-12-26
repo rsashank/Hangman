7e4344b615d772c305aabb72a5cb55b0328b1f92
Sashank Ravipati <110327345+rsashank@users.noreply.github.com>
2022-12-26 12:04:23 +0530

    Made changes to hangman.py

--- hangman.py
+++ hangman.py
@@ -9,8 +9,12 @@ from hangmanart import hangmanstages
 print(hangmanasciiart)
 print("Welcome to Hangman! You have 6 lives, guess the correct word to win.")
 wordblanks=[]
+consonants = ["B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"]
+vowels = ["A","E","I","O","U"]
+letters = []
 lives = 6
 hangmanstage = 0
+flag = 0
 
 for i in range(0,len(word)):
      wordblanks.append("_")
@@ -24,7 +28,27 @@ for j in range(0,26):
         print(wordblanks)
         break
     print(wordblanks)
+    if letters !=[]:
+        print("Used Letters: ", letters)
+    if lives <4 and flag!=1:
+        hint = input(f"Would you like a Hint? You have {lives} lives left. (Yes/No)").upper()
+        if hint == "YES":
+            flag=1
+            vowel=0
+            consonant=0
+            for z in range(0,len(word)):
+                    if word[z] in vowels:
+                        vowel+=1
+                    elif word[z] in consonants:
+                        consonant+=1
+            print(f"Hint: The word has {vowel} vowels and {consonant} consonants.")
     alphabet = input(f"You have {lives} lives left. Guess an alphabet: \n").upper()
+    if alphabet not in consonants + vowels:
+        alphabet = input(f"Please enter an ALPHABET only. You have {lives} lives left.\n").upper() 
+    elif alphabet in letters:
+        print("Used Letters: ", letters)
+        alphabet = input(f"You have already used that alphabet, choose another. You have {lives} lives left.\n").upper()
+    letters.append(alphabet)
     if alphabet not in word:
         lives+=-1
         hangmanstage+=1
